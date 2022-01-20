73bd44ec93c611b1a09a728fc5def33d174ecda2
nathanlauder <laudern42@gmail.com>
2022-01-20 10:44:32 -0600

    generate a password

--- passman/src/actions.rs
+++ passman/src/actions.rs
@@ -1,5 +1,9 @@
-pub fn create(service: &str) {
-    println!("Creating: {:?}", service);
+use rand::Rng; // random functionality
+
+pub fn create(service: &str, length: i32) {
+    println!("Creating {} password of len: {}", service, length);
+    let password: String = generatePassword(length);
+    println!("{}: {}", service, password);
 }
 
 pub fn get(service: &str) {
@@ -12,4 +16,16 @@ pub fn update(service: &str) {
 
 pub fn delete(service: &str) {
     println!("Deleting: {:?}", service);
+}
+
+fn generatePassword(length: i32) -> String {
+    let mut rng = rand::thread_rng();
+    let possibleChars: String = String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*");
+    let mut result: String = String::from("");
+    for _i in 0..length {
+      let index: usize = rng.gen_range(0..possibleChars.len());
+      let character: String = possibleChars.chars().nth(index).unwrap().to_string(); 
+      result.push_str(&character);
+    }
+    result
 }
