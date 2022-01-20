38cee680c2f36dba5675688491cacc01b5a2a63f
nathanlauder <laudern42@gmail.com>
2022-01-19 21:30:50 -0600

    create interface to interact with CRUD

--- pass_man/src/main.rs
+++ pass_man/src/main.rs
@@ -1,3 +1,18 @@
+#![allow(non_snake_case)]
+
+use std::env;
+mod actions;
+
 fn main() {
-    println!("Hello, world!");
+    let cliArgs: Vec<String> = env::args().collect();
+    
+    let service:String = cliArgs[2].to_string();
+    let action:&str = cliArgs[1].as_str();
+    match action {
+        "create" => actions::create(service),
+        "find" => actions::get(service),
+        "update" => actions::update(service),
+        "delete" => actions::delete(service),
+        _ => println!("No action matching {:?}", action)
+    }
 }
